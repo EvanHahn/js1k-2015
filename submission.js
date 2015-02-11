ec5d263df007c64eefaed360049139ad3ac2d395
Evan Hahn <me@evanhahn.com>
2015-02-10 22:44:51 -0600

    nicer movement

--- submission.js
+++ submission.js
@@ -10,6 +10,7 @@
   var centerY = height / 2;
 
   b.style.background = '#111';
+  b.style.cursor = 'crosshair';
   b.innerHTML += '<div id=p style="background:#ff9;width:50px;height:50px;position:absolute;margin-left:-25px;margin-top:-25px">';
 
   var playerX = centerX;
@@ -18,6 +19,8 @@
 
   var playerVelocityX = 0;
   var playerVelocityY = 0;
+  var playerAccelX = 0;
+  var playerAccelY = 0;
 
   b.onmousemove = function(event) {
     playerAngle = Math.atan2(playerY - event.pageY, playerX - event.pageX);
@@ -26,26 +29,33 @@
   b.onkeydown = function(event) {
     var keyCode = event.keyCode;
     if (keyCode == 87) {
-      playerVelocityY = -10;
+      playerAccelY = -1;
     } else if (keyCode == 83) {
-      playerVelocityY = 10;
+      playerAccelY = 1;
     } else if (keyCode == 65) {
-      playerVelocityX = -10;
+      playerAccelX = -1;
     } else if (keyCode == 68) {
-      playerVelocityX = 10;
+      playerAccelX = 1;
     }
   };
 
   b.onkeyup = function(event) {
     var keyCode = event.keyCode;
     if ((keyCode == 87) || (keyCode == 83)) {
-      playerVelocityY = 0;
+      playerAccelY = 0;
     } else if ((keyCode == 65) || (keyCode == 68)) {
-      playerVelocityX = 0;
+      playerAccelX = 0;
     }
   };
 
+  var monsters = [];
+
+  // =========
+  // game loop
+  // =========
+
   var lastTick = 0;
+  var nextInterval = 1;
   function tick(t) {
 
     var dt = (t - lastTick) / 50;
@@ -58,11 +68,31 @@
 
     playerX += playerVelocityX * dt;
     playerY += playerVelocityY * dt;
+    playerVelocityX = handleVelocity(playerVelocityX, playerAccelX, dt);
+    playerVelocityY = handleVelocity(playerVelocityY, playerAccelY, dt);
 
     requestAnimationFrame(tick);
 
   }
 
   tick(0);
+
+  // =========
+  // utilities
+  // =========
+
+  function handleVelocity(velocity, accel, dt) {
+    velocity += accel * dt;
+    if (velocity > 5) {
+      return 5;
+    } else if (velocity < -5) {
+      return -5;
+    } else if (velocity > 0) {
+      return Math.max(velocity - dt * 0.7, 0);
+    } else if (velocity < 0) {
+      return Math.min(velocity + dt * 0.7, 0);
+    }
+    return 0;
+  }
 
 })();

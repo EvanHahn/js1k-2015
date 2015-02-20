e6c3111ef9e06a4b97fadc77e36358e95e8cb41b
Evan Hahn <me@evanhahn.com>
2015-02-20 15:59:28 -0600

    just a little over quota at 1033 chars

--- submission.js
+++ submission.js
@@ -5,6 +5,7 @@
 a = canvases
 b = body
 s = canvas size
+t = text size
 
 I = intervals
 T = tick
@@ -13,39 +14,40 @@ X, Y, Z, and _ are temporary
 
 */
 
-I = 'second minute hour day week month year'.split(' ');
+I = 'sec min hr day wk mon yr'.split(' ');
 
-document.documentElement.style.cssText = 'width:100%;height:100%;display:table;font-family:sans-serif;background-color:#f5f5f9';
+document.documentElement.style.cssText = 'width:100%;height:100%;display:table;background-color:#333';
 b.style.cssText = 'display:table-cell;vertical-align:middle;text-align:center';
 
+t = Infinity;
 a = I.map(function(unit, index) {
 
-  X = document.createElement('div');
-  X.style.display = 'inline-block';
-
   _ = document.createElement('canvas');
-  s = _.width = _.height = innerWidth / 8;
+  s = _.width = _.height = innerWidth / 7;
+  b.style.fontSize = (s / 8) + 'px';
+
+  _.u = unit;
 
   _.m = [1000, 60000, 3600000, 86400000, 604800000, 2629800000, 31557600000][index];
 
   _.e = function(result) {
     result = new Date;
     switch (unit) {
-      case 'year':
+      case 'yr':
         result.setMonth(0);
-      case 'month':
+      case 'mon':
         result.setDate(1);
-      case 'week':
+      case 'wk':
       case 'day':
         result.setHours(0);
-      case 'hour':
+      case 'hr':
         result.setMinutes(0);
-      case 'minute':
+      case 'min':
         result.setSeconds(0);
-      case 'second':
+      case 'sec':
         result.setMilliseconds(0);
     }
-    if (unit === 'week') {
+    if (unit === 'wk') {
       result.setDate(result.getDate() - result.getDay());
     }
     result.setTime(result.getTime() + this.m - 1);
@@ -54,11 +56,18 @@ a = I.map(function(unit, index) {
 
   _.c = _.getContext('2d');
   _.c.fillStyle = _.c.strokeStyle = ['#fa8072', '#faad72', '#fada72', '#acda72', '#87ceeb', '#87aceb', '#a4b7eb'][index];
+  _.c.lineWidth = s * 0.01;
+  _.c.textAlign = 'center';
+  _.c.textBaseline = 'middle';
 
-  X.appendChild(_);
-  X.appendChild(Y = document.createElement('div'));
-  Y.innerHTML = unit;
-  b.appendChild(X);
+  b.appendChild(_);
+
+  $ = 1;
+  do {
+    _.c.font = '100 ' + $ + 'px sans-serif';
+    $ ++;
+  } while (_.c.measureText(unit).width < (s * 0.8));
+  t = Math.min(t, $);
 
   return _;
 
@@ -79,6 +88,9 @@ a = I.map(function(unit, index) {
     canvas.c.moveTo(s / 2, s / 2);
     canvas.c.arc(s / 2, s / 2, s * 0.45, 0, 2 * Math.PI);
     canvas.c.stroke();
+
+    canvas.c.font = '100 ' + t + 'px sans-serif';
+    canvas.c.fillText(canvas.u, s / 2, s / 2);
 
   });
 

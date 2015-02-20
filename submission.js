75cee88ba860d5eb68a6dd7d170f77f5d08b73df
Evan Hahn <me@evanhahn.com>
2015-02-20 14:48:17 -0600

    first version of pietime. buggy

--- submission.js
+++ submission.js
@@ -1,97 +1,74 @@
 // jshint ignore: start
 
-(function() {
+/*
 
-  var width = a.width;
-  var height = a.height;
-  b.removeChild(a);
+a = canvases
+b = body
+s = canvas size
 
-  var centerX = width / 2;
-  var centerY = height / 2;
+I = intervals
+T = tick
 
-  b.style.background = '#111';
-  b.style.cursor = 'crosshair';
-  b.innerHTML += '<div id=p style="background:#ff9;width:50px;height:50px;position:absolute;margin-left:-25px;margin-top:-25px">';
+$ and _ are temporary
 
-  var playerX = centerX;
-  var playerY = centerY;
-  var playerAngle = 0;
+*/
 
-  var playerVelocityX = 0;
-  var playerVelocityY = 0;
-  var playerAccelX = 0;
-  var playerAccelY = 0;
+I = 'second minute hour day week month year'.split(' ');
 
-  b.onmousemove = function(event) {
-    playerAngle = Math.atan2(playerY - event.pageY, playerX - event.pageX);
+a = I.map(function(unit, index) {
+
+  _ = document.createElement('canvas');
+  s = _.width = _.height = innerWidth / 8;
+
+  _.e = function(result) {
+    result = new Date;
+    switch (unit) {
+      case 'year':
+        result.setMonth(0);
+      case 'month':
+        result.setDate(1);
+      case 'week':
+      case 'day':
+        result.setHours(0);
+      case 'hour':
+        result.setMinutes(0);
+      case 'minute':
+        result.setSeconds(0);
+      case 'second':
+        result.setMilliseconds(0);
+    }
+    if (unit === 'week') {
+      result.setDate(result.getDate() - result.getDay() + 7)
+    }
+    return result;
   };
 
-  b.onkeydown = function(event) {
-    var keyCode = event.keyCode;
-    if (keyCode == 87) {
-      playerAccelY = -1;
-    } else if (keyCode == 83) {
-      playerAccelY = 1;
-    } else if (keyCode == 65) {
-      playerAccelX = -1;
-    } else if (keyCode == 68) {
-      playerAccelX = 1;
+  _.m = [31557600000, 2629800000, 604800000, 86400000, 3600000, 60000, 1000][index];
+
+  _.c = _.getContext('2d');
+
+  b.appendChild(_);
+
+  return _;
+
+});
+
+(function T() {
+
+  a.forEach(function(canvas) {
+
+    with (canvas.getContext('2d')) {
+
+      clearRect(0, 0, s, s);
+
+      beginPath();
+      moveTo(s / 2, s / 2);
+      arc(s / 2, s / 2, s * 0.45, 0, 2 * Math.PI * ((canvas.e() - new Date) / canvas.m));
+      fill();
     }
-  };
 
-  b.onkeyup = function(event) {
-    var keyCode = event.keyCode;
-    if ((keyCode == 87) || (keyCode == 83)) {
-      playerAccelY = 0;
-    } else if ((keyCode == 65) || (keyCode == 68)) {
-      playerAccelX = 0;
-    }
-  };
+  });
 
-  var monsters = [];
-
-  // =========
-  // game loop
-  // =========
-
-  var lastTick = 0;
-  var nextInterval = 1;
-
-  (function tick(t) {
-
-    var dt = (t - lastTick) / 50;
-    lastTick = t;
-
-    p.style.transform = 'rotate(' + (playerAngle * 180 / Math.PI) + 'deg)';
-    p.style.left = playerX + 'px';
-    p.style.top = playerY + 'px';
-    p.style.boxShadow = '0 0 ' + ((Math.sin(t / 500) * 10) + 100) + 'px #ffb';
-
-    playerX += playerVelocityX * dt;
-    playerY += playerVelocityY * dt;
-    playerVelocityX = handleVelocity(playerVelocityX, playerAccelX, dt);
-    playerVelocityY = handleVelocity(playerVelocityY, playerAccelY, dt);
-
-    requestAnimationFrame(tick);
-
-  })(0);
-
-  // =========
-  // utilities
-  // =========
-
-  function handleVelocity(velocity, accel, dt) {
-    velocity += accel * dt;
-    if (velocity > 5) {
-      return 5;
-    } else if (velocity < -5) {
-      return -5;
-    } else if (velocity > 0) {
-      return Math.max(velocity - dt * 0.7, 0);
-    } else if (velocity < 0) {
-      return Math.min(velocity + dt * 0.7, 0);
-    }
-    return 0;
-  }
+  requestAnimationFrame(T);
 
 })();

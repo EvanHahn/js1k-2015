3e3ce88437322810e8bb9b5c0cecec33f687c313
Evan Hahn <me@evanhahn.com>
2015-02-20 15:18:30 -0600

    much closer to done

--- submission.js
+++ submission.js
@@ -4,21 +4,32 @@
 
 a = canvases
 b = body
+h = half the canvas size
 s = canvas size
 
 I = intervals
 T = tick
 
-$ and _ are temporary
+X, Y, Z, and _ are temporary
 
 */
 
 I = 'second minute hour day week month year'.split(' ');
 
+document.documentElement.style.cssText = 'width:100%;height:100%;display:table;font-family:sans-serif';
+b.style.cssText = 'display:table-cell;vertical-align:middle;text-align:center';
+
 a = I.map(function(unit, index) {
+
+  X = document.createElement('div');
+  X.style.display = 'inline-block';
 
   _ = document.createElement('canvas');
   s = _.width = _.height = innerWidth / 8;
+
+  _.id = unit;
+
+  _.m = [1000, 60000, 3600000, 86400000, 604800000, 2629800000, 31557600000][index];
 
   _.e = function(result) {
     result = new Date;
@@ -38,34 +49,36 @@ a = I.map(function(unit, index) {
         result.setMilliseconds(0);
     }
     if (unit === 'week') {
-      result.setDate(result.getDate() - result.getDay() + 7)
+      // TODO this is broken
+      result.setDate(result.getDate() - result.getDay() + 7);
     }
+    result.setTime(result.getTime() + this.m - 1);
     return result;
   };
 
-  _.m = [31557600000, 2629800000, 604800000, 86400000, 3600000, 60000, 1000][index];
-
   _.c = _.getContext('2d');
 
-  b.appendChild(_);
+  X.appendChild(_);
+  X.appendChild(Y = document.createElement('div'));
+  Y.innerHTML = unit;
+  b.appendChild(X);
 
   return _;
 
 });
 
+h = s / 2;
+
 (function T() {
 
   a.forEach(function(canvas) {
 
-    with (canvas.getContext('2d')) {
+    canvas.c.clearRect(0, 0, s, s);
 
-      clearRect(0, 0, s, s);
-
-      beginPath();
-      moveTo(s / 2, s / 2);
-      arc(s / 2, s / 2, s * 0.45, 0, 2 * Math.PI * ((canvas.e() - new Date) / canvas.m));
-      fill();
-    }
+    canvas.c.beginPath();
+    canvas.c.moveTo(h, h);
+    canvas.c.arc(s / 2, s / 2, s * 0.45, 0, 2 * Math.PI * ((canvas.e() - new Date) / canvas.m));
+    canvas.c.fill();
 
   });
 
